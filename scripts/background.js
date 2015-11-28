0d6c1a76a7181a02d6f8ee9d703c62041596924d
Bhaskar Tiwari <bt@qbuzzer.com>
2015-11-28 14:43:42 +0530

    Fixed CORS Issue

---
+++ scripts/background.js
@@ -0,0 +1,8 @@
+/*global chrome*/
+(function () {
+    'use strict';
+    chrome.webRequest.onHeadersReceived.addListener(function (response) {
+        response.responseHeaders.push({name: 'Access-Control-Allow-Origin', value: '*'});
+        return {responseHeaders: response.responseHeaders};
+    }, {urls: ["*://*.quoracdn.net/*"]}, ["blocking", "responseHeaders"]);
+}());
