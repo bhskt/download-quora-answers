69dc0d719e95c3bf124738ea688ad03fe37163b3
Bhaskar Tiwari <bt@qbuzzer.com>
2015-11-28 11:50:39 +0530

    Automatically Expands Answers And Then Takes The Image

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-/*global MutationObserver, html2canvas, saveAs, window*/
+/*global MutationObserver, html2canvas, saveAs, window, Node*/
 (function () {
     'use strict';
+    Node.prototype.findParent = function (query) {
+        var parent = this;
+        while (parent && !parent.matches(query)) {
+            parent = parent.parentElement;
+        }
+        return parent;
+    };
     var mutationObserver = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             Array.prototype.slice.call(mutation.addedNodes).forEach(function (addedNode) {
@@ -17,26 +24,39 @@
                                 downloadButton.innerText = 'Download';
                                 downloadButton.onclick = function (event) {
                                     answer.classList.add('download-quora-answers-processing');
+                                    var moreButton = answer.querySelector('.AnswerExpandable:not(.hidden) a.more_link');
                                     var scroll = {x: window.scrollX, y: window.scrollY};
                                     var question;
+                                    function proceed() {
+                                        html2canvas(answer, {
+                                            onrendered: function (canvas) {
+                                                window.scrollTo(scroll.x, scroll.y);
+                                                answer.classList.remove('download-quora-answers-processing');
+                                                if (question) {
+                                                    question.remove();
+                                                    answer = answer.parentElement;
+                                                }
+                                                canvas.toBlob(function (blob) {
+                                                    saveAs(blob, answer.id + '.jpg');
+                                                }, 'image/jpeg', 1);
+                                            }
+                                        });
+                                    }
                                     if (answer.matches('.AnswerBase')) {
                                         question = document.querySelector('.grid_page .header').cloneNode();
                                         question.appendChild(document.querySelector('.grid_page .header .question_text_edit').cloneNode(true));
                                         answer.insertBefore(question, answer.firstChild);
+                                    } else if (moreButton) {
+                                        var expandableObserver;
+                                        expandableObserver = new MutationObserver(function () {
+                                            expandableObserver.disconnect();
+                                            proceed();
+                                        });
+                                        expandableObserver.observe(moreButton.findParent('.AnswerExpandable'), {attributes: true});
+                                        moreButton.click();
+                                    } else {
+                                        proceed();
                                     }
-                                    html2canvas(answer, {
-                                        onrendered: function (canvas) {
-                                            window.scrollTo(scroll.x, scroll.y);
-                                            answer.classList.remove('download-quora-answers-processing');
-                                            if (question) {
-                                                question.remove();
-                                                answer = answer.parentElement;
-                                            }
-                                            canvas.toBlob(function (blob) {
-                                                saveAs(blob, answer.id + '.jpg');
-                                            }, 'image/jpeg', 1);
-                                        }
-                                    });
                                     event.preventDefault();
                                 };
                                 actionBar.appendChild(downloadButton);
